8ec86eb1c4a6d19394d7c0b37789ced1958d4591
AbeKazumasa <katahira0088@gmail.com>
2021-01-03 15:09:28 -0600

    勤怠A完了

---
+++ db/seeds.rb
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+User.create!(name: "管理者",
+  email: "admin@email.com",
+  password: "password",
+  password_confirmation: "password",
+  admin: true)
+
+User.create!(name: "上長A",
+  email: "superior-1@email.com",
+  password: "password",
+  password_confirmation: "password",
+  superior: true)
+
+User.create!(name: "上長B",
+  email: "superior-2@email.com",
+  password: "password",
+  password_confirmation: "password",
+  superior: true)
+
+User.create!(name: "上長C",
+  email: "superior-3@email.com",
+  password: "password",
+  password_confirmation: "password",
+  superior: true)
+
+
+
+
+3.times do |n|
+name  = Faker::Name.name
+email = "sample-#{n+1}@email.com"
+password = "password"
+User.create!(name: name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+    superior: false)
+end
+
